d9ed010dfc3a7be294ea2d00d0d44e197104728e
Scot <bscot.hagen@gmail.com>
2021-06-15 13:56:02 -0700

    finished practice project RazorPagesMovie9

--- RazorPagesMovie9/Models/Movie.cs
+++ RazorPagesMovie9/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,23 @@ namespace RazorPagesMovie9.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z] + [a-zA-Z\s]*$")]
+        [StringLength(30)]
+        [Required]
         public string Genre { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]#$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
     }
 }
